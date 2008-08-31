8d01561894cb78120b76faab211880d83f4eddab
Viktor Ivanov <viktor.r.ivanov@gmail.com>
2008-08-31 16:37:02 -0400

    Simple tests for serving static and php pages.
    A first crack at some unit testing. Using python's unittest module and urllib
    to fetch the contents of a given url and check the mime type and the contents.

---
+++ tests/resources/simple.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>
+            Simple php test.
+        </title>
+    </head>
+    <body>
+        <h1>
+            <? echo "Seems like your server can serve php pages." ?>
+        </h1>
+        <h2>Move on to testing the database.</h2>
+    </body>
+</html>
---
+++ tests/run-tests.py
@@ -0,0 +1,24 @@
+import unittest
+import urllib
+
+class MainTests (unittest.TestCase):
+    def setUp (self):
+        resources = "http://localhost:3000/tests/resources/"
+        self.static_url = urllib.urlopen (resources+"static.html")
+        self.simple_php_url = urllib.urlopen (resources+"simple.php")
+
+    def test_mimetype (self):
+        self.assertEqual (self.static_url.headers.type, 'text/html')
+
+    def test_static_content (self):
+        content = self.static_url.read ()
+        pos = content.find ("Seems like your server can serve")
+        self.assertEqual (pos > 0, True)
+
+    def test_simple_php (self):
+        content = self.simple_php_url.read ()
+        pos = content.find ("Seems like your server can serve")
+        self.assertEqual (pos > 0, True)
+
+if __name__ == '__main__':
+    unittest.main()
