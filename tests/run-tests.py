bbdcb96a936f7aeabb83cdece59acea726cafbbf
Viktor Ivanov <viktor.r.ivanov@gmail.com>
2008-09-02 09:47:09 -0400

    Make the sqlite test part of the automated testing.
    Move the sqlite.php test into resources/ where the other tests are and add a
    few lines in run-tests.py to run it and check the output.

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -6,6 +6,7 @@ class MainTests (unittest.TestCase):
         resources = "http://localhost:3000/tests/resources/"
         self.static_url = urllib.urlopen (resources+"static.html")
         self.simple_php_url = urllib.urlopen (resources+"simple.php")
+        self.sqlite_php_url = urllib.urlopen (resources+"sqlite.php")
 
     def test_mimetype (self):
         self.assertEqual (self.static_url.headers.type, 'text/html')
@@ -18,6 +19,11 @@ class MainTests (unittest.TestCase):
     def test_simple_php (self):
         content = self.simple_php_url.read ()
         pos = content.find ("Seems like your server can serve")
+        self.assertEqual (pos > 0, True)
+
+    def test_sqlite_php (self):
+        content = self.sqlite_php_url.read ()
+        pos = content.find ("finished successfully")
         self.assertEqual (pos > 0, True)
 
 if __name__ == '__main__':
